804fa43eb5e9a5c09587197368b277d5dc2b17ae
B L <67735440+123BL321@users.noreply.github.com>
2024-09-07 14:06:04 -0400

    Update web_scraping_with_api.py

--- web_scraping_with_api.py
+++ web_scraping_with_api.py
@@ -2,41 +2,6 @@ from pygooglenews import GoogleNews
 from bs4 import BeautifulSoup
 import requests
 
-
-
-"""""
-def get_titles(search):
-    search = gn.search(search)
-
-    #organizes by entries where titles and more can be extracted 
-    for item in search['entries']:
-        print(item.title)
-        titles
-    return 
-"""""
-
-#def get_description(link):
-    # import the required libraries
-    #base_URL = 'https://www.gamespot.com'
- 
-# get the news category
-#category_URL = f'{base_URL}/news'
- 
-#response = requests.get(category_URL)
- 
-#data_links  = []
- 
-#if response.status_code == 200:
- 
-    # parse HTML content with BeautifulSoup
-    #soup = BeautifulSoup(response.content, 'html.parser')
-    #print(soup)
- 
-#else:
-    #print(f'An error has occurred with status {response.status_code}')
-
-
-
 def organize_information(search, location):
     gn = GoogleNews(lang = 'en', country = location)
     search = gn.search(search)
